bb468cf2d952b2f50e9107476b252122ee2dc81a
shantanu3438 <shantanuanand@live.in>
2023-12-26 11:13:41 +0530

    Now to do items can be added and they show up at bottom

--- src/Components/ToDoBody.jsx
+++ src/Components/ToDoBody.jsx
@@ -1,13 +1,44 @@
+import { useState } from "react"
+
 export default function ToDoBody() {
+
+    const [items, setItems] = useState([])
+    const [newItemText, setNewItemText] = useState('')
+
+    const handleInputChange = (event) =>{
+        setNewItemText(event.target.value)
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key == "Enter" && newItemText.trim() !== "") {
+            const newItem = {
+                title: newItemText,
+                id: Date.now().toString(),
+            }
+            setItems([...items, newItem])
+            setNewItemText('')
+        }
+    } 
+
     return (
         <div className="ToDoBody">
             <div>
                 <div className="ToDoBody--addItem">
                     <input type="checkbox"></input>
-                    <input type="textbox" className="addItem--text" id="addItem" placeholder="Add a new To Do item"></input>
+                    <input 
+                    type="text" 
+                    className="addItem--text" 
+                    id="addItem" 
+                    placeholder="Add a new To Do item"
+                    value={newItemText}
+                    onKeyDown={handleKeyDown}
+                    onChange={handleInputChange}
+                    ></input>
                 </div>
                 <div className="ToDoBody--items">
-                <ToDoItem title="Buy Milk" id="1"/>
+                {items.map(item => (
+                    <ToDoItem key={item.id} title={item.title} id={item.id} />
+                ))}
                 </div>
             </div>
         </div>
